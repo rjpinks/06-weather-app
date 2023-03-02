bf6b1200b6c3b78427cb68640c35f32e6f63c4e1
Ryan <pinkstonr54@gmail.com>
2023-03-01 23:27:50 -0600

    1st commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+
+
+
+
+
+
+/*
+PSEUDOCODE
+
+PHASE 1:
+When I enter a city into the value field of the form and submit,
+    an eventlistener fires and the app fetches data from the API
+    it will json the data in .then()s
+    once I have the data accessable the next phase begins
+
+PHASE2:
+The data the I fetched will be posted into 2 different sections. Current day/info & the 5-day forecast
+    to do this I will need to do a for loop. if index === 0 post in current. else post in 5 day. (actually need will need to look at the data i get from API for better judgement)
+    I will need to do the 3 steps of adding to the DOM
+    This should be plenty to work on for now.
+*/

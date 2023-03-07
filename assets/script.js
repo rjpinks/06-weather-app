a1ccfd8e30c8aa3e30c15f73afe237e4c35cd141
Ryan <pinkstonr54@gmail.com>
2023-03-07 03:53:45 -0600

    got the second fetch working, can access data from both APIs

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,21 @@
 var searchBox = document.querySelector("#search-box");
 var weatherCallUrl = "";
-// function weatherCall() {
-//     //this function needs to make the api call for the actual weather
-//     var weatherCallUrl = "api.openweathermap.org/data/2.5/forecast?lat=" + dataList[0] + "&lon=" + dataList[1] + "&appid=e03ff718bfde42745e7d19afe3ccf6bc";
-//     console.log(weatherCallUrl);
-// }
+
+function secondFetch () {
+    fetch(weatherCallUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //this loop will need to give me the 5 day forecast n whatnot.
+            console.log("lol wut");
+            for (let index = 0; index < data.list.length; index++) {
+                console.log(data.list[index].main.temp);
+                console.log(data.list[index].weather[0].main);
+                console.log(data.list[index].weather[0].description);
+            }
+        })
+}
 
 searchBox.addEventListener("submit", function(event) {
     event.preventDefault();
@@ -13,36 +24,21 @@ searchBox.addEventListener("submit", function(event) {
     var searchedCity = document.querySelector("#city-search").value;
     var cityApiCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + searchedCity + "&limit=1&appid=e03ff718bfde42745e7d19afe3ccf6bc";
 
-
     fetch(cityApiCall)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            //this for loop needs to return the lat and long of the city
+            //this for loop needs to return the lat and long of the city. it's only one point returned to me, but it's good practice.
             for (let index = 0; index < data.length; index++) {
                 var cityData = data[index];
                 var dataList = [cityData.lat, cityData.lon];
-                weatherCallUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + dataList[0] + "&lon=" + dataList[1] + "&appid=e03ff718bfde42745e7d19afe3ccf6bc";
-                console.log("in function" ,weatherCallUrl);
+                weatherCallUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=" + dataList[0] + "&lon=" + dataList[1] + "&units=imperial" + "&appid=e03ff718bfde42745e7d19afe3ccf6bc";
+                //console.log("in function" ,weatherCallUrl);
                 return weatherCallUrl;
             };
-            console.log("outside function", weatherCallUrl);
-            return weatherCallUrl;
         })
-    console.log("does it work?", weatherCallUrl);
-    })
-//     fetch(weatherCallUrl)
-//         .then(function (response){
-//            return response.json(); 
-//         })
-//         .then(function (data) {
-
-//         })
-// })
-
-
-
+})
 
 /*
 PSEUDOCODE
